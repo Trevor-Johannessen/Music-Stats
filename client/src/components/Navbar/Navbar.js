554010c52776e79e5c35c4084873d03df7c584ac
Trevor Johannessen <trevor.johannessen@gmail.com>
2022-12-20 14:25:04 -0500

    Added Text and Icons to Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import './style.css';
-import Typography from '@mui/material/Typography';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
 
@@ -12,7 +13,9 @@ const Navbar = () => {
 
     return (
         <div id='navbar'>
-            
+            <span id="title">WEBSITE TITLE</span>
+            <SettingsIcon style={{width: '5vw', height: '5vh'}}/>
+            <MenuIcon style={{width: '5vw', height: '5vh'}}/>
         </div>
     )
 }
